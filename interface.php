ad21ae37e6df58f5db9e6c3221a3d8df9bc93cdd
benjaminsandras <bensand77@hotmail.fr>
2018-08-17 16:56:49 +0200

    coucou

--- interface.php
+++ interface.php
@@ -53,7 +53,7 @@
 	//on se connecte à mysql :
 	try  
 	{
-		$bdd = new PDO('mysql:host=localhost;dbname=canards;charset=utf8', 'annie', '12345678');
+		$bdd = new PDO('mysql:host=localhost;dbname=adeline;charset=utf8', 'benji', 'aqwsedcft7777');
 	}
 	// en cas d'erreur on affiche un message :
 	catch (Exception $e)
@@ -74,6 +74,20 @@
 
 	?>
 
+	<form method="post">
+		<div class="input-group">
+			<select class="custom-select" name="trier" aria-label="Example select with button addon">
+				<option value="trier1">Nombre croissant</option>
+				<option value="trier2">Nombre décroissant</option>
+				<option value="trier3">Ville</option>
+				<option value="trier4">Le plus récent</option>
+				<option value="trier5">Le plus ancien</option>
+			</select>
+			<div class="input-group-append">
+				<button class="btn btn-outline-secondary" type="submit">Valider</button>
+			</div>
+		</div>
+	</form>
 
 	<table class="table"> 
 		<thead>
@@ -84,36 +98,127 @@
 		</thead>
 
 
-		<?php 
-		$reponse = $bdd->query('SELECT * FROM recensement');
-		while($donnees=$reponse->fetch())
-		{
-			echo '<tr><td>'.$donnees['date'].'</td><td>'.$donnees['heure'].'</td><td>'.$donnees['lieu'].'</td><td>'.$donnees['nombre'].'</td></tr>';
-		}
-		?>
+		<?php
+		$tri = $_POST['trier'];                    
 
+		if(empty($tri))
+		{
+			$tri = "trier1";
+		}
+		if ($tri == "trier1")
+		{
+
+			$reponse = $bdd->query('SELECT * FROM recensement ORDER BY nombre') ;
+			while($donnees=$reponse->fetch())
+			{
+				echo '<tr>
+				<td>'.$donnees['date'].'</td>
+				<td>'.$donnees['heure'].'</td>
+				<td>'.$donnees['lieu'].'</td>
+				<td>'.$donnees['nombre'].'</td>
+				</tr>';
+			}
+		}                    
+
+		if ($tri == "trier2")
+		{
+
+			$reponse = $bdd->query('SELECT * FROM recensement ORDER BY nombre DESC') ;
+			while($donnees=$reponse->fetch())
+			{
+				echo '<tr>
+				<td>'.$donnees['date'].'</td>
+				<td>'.$donnees['heure'].'</td>
+				<td>'.$donnees['lieu'].'</td>
+				<td>'.$donnees['nombre'].'</td>
+				</tr>';
+			}
+		}                   
+
+		if ($tri == "trier3")
+		{
+
+			$reponse = $bdd->query('SELECT * FROM recensement ORDER BY lieu ASC') ;
+			while($donnees=$reponse->fetch())
+			{
+				echo '<tr>
+				<td>'.$donnees['date'].'</td>
+				<td>'.$donnees['heure'].'</td>
+				<td>'.$donnees['lieu'].'</td>
+				<td>'.$donnees['nombre'].'</td>
+				</tr>';
+			}
+		}                    
+
+		if ($tri == "trier4")
+		{
+
+			$reponse = $bdd->query('SELECT * FROM recensement ORDER BY date ASC') ;
+			while($donnees=$reponse->fetch())
+			{
+				echo '<tr>
+				<td>'.$donnees['date'].'</td>
+				<td>'.$donnees['heure'].'</td>
+				<td>'.$donnees['lieu'].'</td>
+				<td>'.$donnees['nombre'].'</td>
+				</tr>';
+			}
+		}                    
+
+
+
+		if ($tri == "trier5")
+		{
+
+			$reponse = $bdd->query('SELECT * FROM recensement ORDER BY date DESC') ;
+			while($donnees=$reponse->fetch())
+			{
+				echo '<tr>
+				<td>'.$donnees['date'].'</td>
+				<td>'.$donnees['heure'].'</td>
+				<td>'.$donnees['lieu'].'</td>
+				<td>'.$donnees['nombre'].'</td>
+				</tr>';
+			}
+
+		}
+	
+		?>
+	
 	</table> 
 
-
-
 	<form id="formulaire" method="post" action="">
+
+
 		<h2 id="titreform">J'ai aperçu un/plusieurs volatile(s) !</h2>
 
-
-		<label class="form" for="lieu">Lieu :</label>
-		<input class="form" type="text" name="lieu" placeholder="ex : Auch">
-
-		<label class="form" for="date">Date :</label>
-		<input class="form" type="date" name="date" placeholder="ex : 16/08/2018">
-
-		<label class="form" for="heure">Heure :</label>
-		<input class="form" type="time" name="heure" placeholder="ex : 16h30">
-
-		<label class="form" for="nombre">Nombre :</label>
-		<input class="form" type="number" name="nombre" placeholder="ex : 3">
+		<div class="form-group row">
+			<label class="col-sm col-form-label" for="lieu">Lieu :</label>
+			<div class="col-sm">
+				<input class="form-control" type="text" name="lieu" placeholder="ex : Auch">
+			</div>
+		</div>
+		<div class="form-group row">
+			<label class="col-sm col-form-label" for="date">Date :</label>
+			<div class="col-sm">
+				<input class="form-control" type="date" name="date" placeholder="ex : 16/08/2018">
+			</div>
+		</div>
+		<div class="form-group row">
+			<label class="col-sm col-form-label" for="heure">Heure :</label>
+			<div class="col-sm">
+				<input class="form-control" type="time" name="heure" placeholder="ex : 16h30">
+			</div>
+		</div>
+		<div class="form-group row">
+			<label class="col-sm col-form-label" for="nombre">Nombre :</label>
+			<div class="col-sm">
+				<input class="form-control" type="number" name="nombre" placeholder="ex : 3">
+			</div>
+		</div>
 
 		<input id="valider" class="form" type="submit" name="ok" value="Envoyer">
-		<a href="interface.php"><button class="form" >J'ai confondu, ce n'était pas un canard !</button></a>
+		<a href="interface.php"><button class="btn btn-primary btn-lg btn-block" >J'ai confondu, ce n'était pas un canard !</button></a>
 	</form>
 
 	<script type="text/javascript" src="jquery.min.js"></script>
@@ -121,24 +226,27 @@
 
 	<script type="text/javascript">
 
-		$('#valider').click(function()
+		
+		function callback()
 		{
-			$('#presentation').show();
-			callback()
+			$('#presentation').hide();
+		};
+
+		$('#valider').click(function firstclick()
+		{
+			$('#presentation').empty();
+			// $('#presentation').show();
+			callback();
 		});
 		
-		$('#btnbtn').click(function(callback)
+		$('#btnbtn').click(function zakiaclick(callback)
 		{
-			// $('#presentation').hide();
+			$('#presentation').hide();
 			$('.table').hide();
 			$('#formulaire').show();
 			callback();
 		});
 
-		function callback()
-		{
-			$('#presentation').hide();
-		};
 
 
 
